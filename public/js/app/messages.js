733eeb60ee7d81ac1379a7fabb4f799e3df692db
Two <njsolomonoh@gmail.com>
2021-04-12 18:37:53 -0400

    FIX: Fixed bugs on markdown.

--- public/js/app/messages.js
+++ public/js/app/messages.js
@@ -17,19 +17,18 @@ function parseEmojis(text) {
     let newtext = "";
 
     while (oldtext.length !== 0) {
-        if (oldtext.startsWith("\\")) {
+        let emoji = findEmoji(oldtext);
+
+        if (oldtext.startsWith("\\") && emoji) {
             oldtext = oldtext.slice(1);
             newtext = newtext + oldtext.slice(0, 1);
             oldtext = oldtext.slice(1);
+        } else if (emoji) {
+            newtext = newtext + emoji.emoji;
+            oldtext = oldtext.slice(emoji.text.length);
         } else {
-            let emoji = findEmoji(oldtext);
-            if (emoji) {
-                newtext = newtext + emoji.emoji;
-                oldtext = oldtext.slice(emoji.text.length);
-            } else {
-                newtext = newtext + oldtext.slice(0, 1);
-                oldtext = oldtext.slice(1);
-            };
+            newtext = newtext + oldtext.slice(0, 1);
+            oldtext = oldtext.slice(1);
         }
     };
 
@@ -294,8 +293,16 @@ function textParser(text) {
     while (oldtext.length !== 0) {
         if (oldtext.startsWith("\\")) {
             oldtext = oldtext.slice(1);
-            newtext = newtext + oldtext.slice(0, 1);
-            oldtext = oldtext.slice(1);
+
+            if (Array.isArray(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)) && /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext).length !== 0) { // https://stackoverflow.com/questions/43242440/javascript-unicode-emoji-regular-expressions
+                let emojiparsed = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0];
+
+                newtext = newtext + emojiparsed;
+                oldtext = oldtext.slice(emojiparsed.length);
+            } else {
+                newtext = newtext + oldtext.slice(0, 1);
+                oldtext = oldtext.slice(1);
+            }
         } else {
             if (newtext == "" && oldtext.startsWith("> ") || oldtext.startsWith("\n> ")) {
                 newtext = newtext + `<div class="blockquoteContainer-U5TVEi"><div class="blockquoteDivider-2hH8H6"></div><blockquote>`;
@@ -373,6 +380,11 @@ function textParser(text) {
                     newtext = newtext + "</span></span>";
                 };
                 oldtext = oldtext.slice(2);
+            } else if (Array.isArray(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)) && /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0].length !== 0) { // https://stackoverflow.com/questions/43242440/javascript-unicode-emoji-regular-expressions
+                let emojiparsed = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0];
+
+                newtext = newtext + twemoji.parse(emojiparsed);
+                oldtext = oldtext.slice(emojiparsed.length);
             } else if (oldtext.startsWith(":pyrodev:")) {
                 newtext = newtext + `<img class="emoji" draggable="false" alt=":pyrodev:" src="https://cdn.discordapp.com/emojis/829584881461493781.png?v=1">`;
                 oldtext = oldtext.slice(`:pyrodev:`.length);
@@ -395,7 +407,7 @@ function textParser(text) {
     if (markdown.quote == true) newtext = newtext + "</blockquote></div>";
     if (markdown.bigcodeblock == true) newtext = removeLastOfThis(newtext, `<pre><code class="scrollbarGhostHairline-1mSOM1 scrollbar-3dvm_9 hljs">`, "```");
 
-    return twemoji.parse(createTextLinks_(newtext));
+    return createTextLinks_(newtext);
 };
 
 function removeLastOfThis(text, find, replace) {
