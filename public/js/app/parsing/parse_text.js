e7d553ed0755b15e0bf9412f77fc18f2e3b80184
Two <njsolomonoh@gmail.com>
2021-04-13 18:00:59 -0400

    Made custom emotes jumbo.

--- public/js/app/parsing/parse_text.js
+++ public/js/app/parsing/parse_text.js
@@ -194,11 +194,11 @@ function parseText(text) {
                 oldtext = oldtext.slice(emojiparsed.length);
 
             } else if (oldtext.startsWith(":pyrodev:")) {
-                newtext = newtext + `<img class="emoji" draggable="false" alt=":pyrodev:" src="https://cdn.discordapp.com/emojis/829584881461493781.png?v=1">`;
+                newtext = newtext + `<img class="emoji" draggable="false" alt=":pyrodev:" src="https://cdn.discordapp.com/emojis/829584881461493781.png?v=1" class="${onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"}>`;
                 oldtext = oldtext.slice(`:pyrodev:`.length);
 
             } else if (oldtext.startsWith(":firebase:")) {
-                newtext = newtext + `<img class="emoji" draggable="false" alt=":firebase:" src="https://cdn.discordapp.com/emojis/811084920255807518.gif?v=1">`;
+                newtext = newtext + `<img class="emoji" draggable="false" alt=":firebase:" src="https://cdn.discordapp.com/emojis/811084920255807518.gif?v=1" class="${onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"}">`;
                 oldtext = oldtext.slice(`:firebase:`.length);
 
             } else {
