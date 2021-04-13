a3529dcad0c9ca7fb2eda60266974299aa53d58b
Glowbase <45991010+glowbase@users.noreply.github.com>
2021-04-13 10:42:44 +1000

    REF: Refactored parsing

---
+++ public/js/app/parsing/parse_embeds.js
@@ -0,0 +1,42 @@
+//? ------------------------------------------------------------------------------------
+//?
+//?  /app/parsing/parse_embeds.js
+//?  Pyro Chat
+//?
+//?  Developed by Robolab LLC
+//?  Copyright (c) 2021 Robolab LLC. All Rights Reserved
+//?     
+//? ------------------------------------------------------------------------------------
+
+
+/**
+ * 
+ * @param {*} embeds 
+ * @returns 
+ */
+function parseEmbeds(embeds) {
+    const htmls = [];
+
+    for (let embed of embeds) {
+        const borderColour = embed.color ? ` style="border-color: ${embed.color};"` : '';
+        let html = `<div class="container-1ov-mD"><div class="embedWrapper-lXpS3L embedFull-2tM8-- embed-IeVjo6 markup-2BOw-j" aria-hidden="false" ${borderColour}><div class="grid-1nZz7S">`;
+        
+        if (embed.title) {
+            html += `<div class="embedTitle-3OXDkz embedMargin-UO5XwE">${parseText(embed.title)}</div>`;
+        }
+
+        if (embed.author) {
+            html += `<div class="embedAuthor-3l5luH embedMargin-UO5XwE"><span class="embedAuthorName-3mnTWj">${parseText(embed.author)}</span></div>`;
+        }
+
+        if (embed.description) {
+            html += `<div class="embedDescription-1Cuq9a embedMargin-UO5XwE">${parseText(embed.description)}</div>`
+        }
+
+        html = html + '</div></div>';
+
+        htmls.push(html);
+    };
+
+    return htmls.join("\n");
+};
---
+++ public/js/app/parsing/parse_emoji.js
@@ -0,0 +1,57 @@
+//? ------------------------------------------------------------------------------------
+//?
+//?  /app/parse_emoji.js
+//?  Pyro Chat
+//?
+//?  Developed by Robolab LLC
+//?  Copyright (c) 2021 Robolab LLC. All Rights Reserved
+//?     
+//? ------------------------------------------------------------------------------------
+
+
+/**
+ * 
+ * @param {*} text 
+ * @returns 
+ */
+ function parseEmojis(text) {
+    let oldtext = text;
+    let newtext = "";
+
+    while (oldtext.length !== 0) {
+        const emoji = findEmoji(oldtext);
+
+        if (oldtext.startsWith("\\") && emoji) {
+            oldtext = oldtext.slice(1);
+            newtext = newtext + oldtext.slice(0, 1);
+            oldtext = oldtext.slice(1);
+        } else if (emoji) {
+            newtext = newtext + emoji.emoji;
+            oldtext = oldtext.slice(emoji.text.length);
+        } else {
+            newtext = newtext + oldtext.slice(0, 1);
+            oldtext = oldtext.slice(1);
+        }
+    };
+
+    return newtext;
+};
+
+
+/**
+ * 
+ * @param {*} text 
+ * @returns 
+ */
+function findEmoji(text) {
+    for (let [name, emoji] of Object.entries(emojis)) {
+        if (text.startsWith(`:${name}:`)) {
+            return {
+                text: `:${name}:`,
+                emoji: emoji
+            };
+        };
+    };
+
+    return null;
+};
