68dd93f76d0eb6b4eacce3977992b3eb843463ca
Two <njsolomonoh@gmail.com>
2021-04-13 09:11:38 -0400

    Update parse_text.js

--- public/js/app/parsing/parse_text.js
+++ public/js/app/parsing/parse_text.js
@@ -66,7 +66,6 @@ function removeLastOf(text, find, replace) {
     return removedlast;
 };
 
-
 /**
  * 
  * @param {*} text 
@@ -86,6 +85,13 @@ function parseText(text) {
         quote: false,
         bigcodeblock: false
     };
+
+    // https://stackoverflow.com/questions/56504602/check-if-string-contains-only-emojis-javascript/56504667 
+    
+    let onlyemojis = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+$/.test(oldtext.replace(/ /g, ""));
+
+    let emojicount = 0;
+    oldtext.replace(/[\u{1F300}-\u{1F6FF}]/gu, () => emojicount++);
 
     while (oldtext.length !== 0) {
         if (oldtext.startsWith("\\")) {
@@ -195,7 +201,9 @@ function parseText(text) {
             } else if (Array.isArray(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)) && /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0].length !== 0) { // https://stackoverflow.com/questions/43242440/javascript-unicode-emoji-regular-expressions
                 const emojiparsed = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0];
 
-                newtext = newtext + twemoji.parse(emojiparsed);
+                newtext = newtext + twemoji.parse(emojiparsed, {
+                    className: onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"
+                });
                 oldtext = oldtext.slice(emojiparsed.length);
 
             } else if (oldtext.startsWith(":pyrodev:")) {
