1a10917fd81800ea91ab2c8eb436ad0bf1b2190f
Two <njsolomonoh@gmail.com>
2021-05-18 22:31:20 -0400

    untested, hope this works too

--- public/js/app/parsing/text.js
+++ public/js/app/parsing/text.js
@@ -87,32 +87,32 @@ function parseText(text) {
             if (Array.isArray(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)) && /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0].length !== 0) { // https://stackoverflow.com/questions/43242440/javascript-unicode-emoji-regular-expressions
                 const emojiparsed = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0];
 
-                newtext = newtext + emojiparsed;
+                newtext += emojiparsed;
                 oldtext = oldtext.slice(emojiparsed.length);
             } else {
-                newtext = newtext + oldtext.slice(0, 1);
+                newtext += oldtext.slice(0, 1);
                 oldtext = oldtext.slice(1);
             }
 
         } else {
             if (newtext == "" && oldtext.startsWith("&gt; ") || oldtext.startsWith("\n&gt; ")) {
                 console.log(true)
-                newtext = newtext + `<div class="blockquoteContainer-U5TVEi"><div class="blockquoteDivider-2hH8H6"></div><blockquote>`;
+                newtext += `<div class="blockquoteContainer-U5TVEi"><div class="blockquoteDivider-2hH8H6"></div><blockquote>`;
                 oldtext = oldtext.slice(oldtext.startsWith("\n&gt; ") ? "\n&gt; ".length : "&gt; ".length);
                 markdown.quote = true;
 
             } else if (oldtext.startsWith("\n") && markdown.quote == true) {
-                newtext = newtext + "</blockquote></div>"
+                newtext += "</blockquote></div>"
                 oldtext = oldtext.slice(1);
                 markdown.quote = false;
 
             } else if (oldtext.startsWith("**") && !oldtext.startsWith("****")) {
                 if (markdown.bold == false) {
                     markdown.bold = true;
-                    newtext = newtext + "<b>";
+                    newtext += "<b>";
                 } else {
                     markdown.bold = false;
-                    newtext = newtext + "</b>";
+                    newtext += "</b>";
                 }
 
                 oldtext = oldtext.slice(2);
@@ -120,10 +120,10 @@ function parseText(text) {
             } else if (oldtext.startsWith("__") && !oldtext.startsWith("____")) {
                 if (markdown.underlined == false) {
                     markdown.underlined = true;
-                    newtext = newtext + "<u>";
+                    newtext += "<u>";
                 } else {
                     markdown.underlined = false;
-                    newtext = newtext + "</u>";
+                    newtext += "</u>";
                 }
 
                 oldtext = oldtext.slice(2);
@@ -131,10 +131,10 @@ function parseText(text) {
             } else if (oldtext.startsWith("*") && !oldtext.startsWith("**")) {
                 if (markdown.italicized == false) {
                     markdown.italicized = true;
-                    newtext = newtext + "<em>";
+                    newtext += "<em>";
                 } else {
                     markdown.italicized = false;
-                    newtext = newtext + "</em>";
+                    newtext += "</em>";
                 }
 
                 oldtext = oldtext.slice(1);
@@ -142,12 +142,12 @@ function parseText(text) {
             } else if (oldtext.startsWith("```") && !oldtext.startsWith("``````")) {
                 if (markdown.bigcodeblock == true) {
                     markdown.bigcodeblock = false;
-                    newtext = newtext + `</code></pre>`;
+                    newtext += `</code></pre>`;
 
                     oldtext = oldtext.slice(3);
                 } else {
                     markdown.bigcodeblock = true;
-                    newtext = newtext + `<pre><code class="scrollbarGhostHairline-1mSOM1 scrollbar-3dvm_9 hljs">`;
+                    newtext += `<pre><code class="scrollbarGhostHairline-1mSOM1 scrollbar-3dvm_9 hljs">`;
 
                     oldtext = oldtext.slice(3);
 
@@ -157,10 +157,10 @@ function parseText(text) {
             } else if (oldtext.startsWith("`") && !oldtext.startsWith("``")) {
                 if (markdown.codeblock == false) {
                     markdown.codeblock = true;
-                    newtext = newtext + "<code>";
+                    newtext += "<code>";
                 } else {
                     markdown.codeblock = false;
-                    newtext = newtext + "</code>";
+                    newtext += "</code>";
                 }
 
                 oldtext = oldtext.slice(1);
@@ -168,10 +168,10 @@ function parseText(text) {
             } else if (oldtext.startsWith("~~") && !oldtext.startsWith("~~~~")) {
                 if (markdown.strikethrough == false) {
                     markdown.strikethrough = true;
-                    newtext = newtext + "<del>";
+                    newtext += "<del>";
                 } else {
                     markdown.strikethrough = false;
-                    newtext = newtext + "</del>";
+                    newtext += "</del>";
                 }
 
                 oldtext = oldtext.slice(2);
@@ -179,33 +179,33 @@ function parseText(text) {
             } else if (oldtext.startsWith("||") && !oldtext.startsWith("||||")) {
                 if (markdown.spoiler == false) {
                     markdown.spoiler = true;
-                    newtext = newtext + `<span class="spoilerText-3p6IlD hidden-HHr2R9" aria-expanded="false" tabindex="0" role="button" aria-label="Spoiler"><span class="inlineContent-3ZjPuv">`
+                    newtext += `<span class="spoilerText-3p6IlD hidden-HHr2R9" aria-expanded="false" tabindex="0" role="button" aria-label="Spoiler"><span class="inlineContent-3ZjPuv">`
                 } else {
                     markdown.spoiler = false;
-                    newtext = newtext + "</span></span>";
+                    newtext += "</span></span>";
                 }
                 oldtext = oldtext.slice(2);
 
             } else if (Array.isArray(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)) && /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0].length !== 0) { // https://stackoverflow.com/questions/43242440/javascript-unicode-emoji-regular-expressions
                 const emojiparsed = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}]*/ug.exec(oldtext)[0];
 
-                newtext = newtext + twemoji.parse(emojiparsed, {
+                newtext += twemoji.parse(emojiparsed, {
                     className: onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"
                 });
                 oldtext = oldtext.slice(emojiparsed.length);
 
             /*
             } else if (oldtext.startsWith(":pyrodev:")) {
-                newtext = newtext + `<img class="emoji" draggable="false" alt=":pyrodev:" src="https://cdn.discordapp.com/emojis/829584881461493781.png?v=1" class="${onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"}>`;
+                newtext += `<img class="emoji" draggable="false" alt=":pyrodev:" src="https://cdn.discordapp.com/emojis/829584881461493781.png?v=1" class="${onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"}>`;
                 oldtext = oldtext.slice(`:pyrodev:`.length);
 
             } else if (oldtext.startsWith(":firebase:")) {
-                newtext = newtext + `<img class="emoji" draggable="false" alt=":firebase:" src="https://cdn.discordapp.com/emojis/811084920255807518.gif?v=1" class="${onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"}">`;
+                newtext += `<img class="emoji" draggable="false" alt=":firebase:" src="https://cdn.discordapp.com/emojis/811084920255807518.gif?v=1" class="${onlyemojis && emojicount <= 27 ? "emoji jumboable" : "emoji"}">`;
                 oldtext = oldtext.slice(`:firebase:`.length);
             */
 
             } else {
-                newtext = newtext + oldtext.slice(0, 1);
+                newtext += oldtext.slice(0, 1);
                 oldtext = oldtext.slice(1);
 
             }
